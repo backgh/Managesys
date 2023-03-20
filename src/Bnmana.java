3e1b60cf227581c40a667605b2c4a17b0e3730d6
backgh <ace020505@gmail.com>
2023-03-20 12:17:12 +0900

    added managesys

---
+++ src/Bnmana.java
@@ -0,0 +1,9 @@
+
+public class Bnmana {
+
+	public static void main(String[] args) {
+		System.out.println("1.Add Book Number 2.Delete Book Number 3.Edit Book Number 4.View Book Number 5.Exit");
+
+	}
+
+}
