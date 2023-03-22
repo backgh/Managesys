266de810ed6d7444d6116ac8555ce41a1330b6b4
backgh <ace020505@gmail.com>
2023-03-22 10:40:38 +0900

    week 3 blabla

--- src/Bnmana.java
+++ src/Bnmana.java
@@ -1,9 +1,39 @@
+import java.util.Scanner;
 
 public class Bnmana {
-
 	public static void main(String[] args) {
-		System.out.println("1.Add Book Number 2.Delete Book Number 3.Edit Book Number 4.View Book Number 5.Exit");
+		
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+		
+		while (num !=6) {
+			System.out.println("1. Add Book Number ");
+			System.out.println("2. Delete Book Number ");
+			System.out.println("3. Exit Book Number ");
+			System.out.println("4. View Book Number ");
+			System.out.println("5. Show a Menu ");
+			System.out.println("6. Exit ");
+			System.out.println("Select one number between 1 ~ 5:");
+			num = input.nextInt();
+			switch(num) {
+			case 1:
+				System.out.println("Book Number: ");
+				int BNu = input.nextInt();
+				System.out.println("Book Name: ");
+				String BNa = input.next();
+				break;
+			case 2:
+			case 3:
+			case 4:
+				System.out.println("Book Number: ");
+				int Bnu2 = input.nextInt();
+			case 5:
+			case 6:
+			}
+			
+			
+		}
 
-	}
+}
 
 }
